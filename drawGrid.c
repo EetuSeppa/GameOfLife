7477cee23bdd81d038c52ad35bfd7268ad0b4bf5
EetuSeppa <eetu.seppanen99@gmail.com>
2020-12-19 09:25:37 +0200

    Added grid drawing

--- drawGrid.c
+++ drawGrid.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include "raylib.h"
 
-int main (void) {
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 700
+int main (void) {		
+	int lineDistance, i, linePos;
+	lineDistance = 10;
 
+	//Lisää paneroitujen koordinaattien muuttaminen näytölle
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game Of Life");
 
+	while (!WindowShouldClose()) {
+	
+	BeginDrawing();
+		ClearBackground(RAYWHITE);
+		for (linePos = 0;  linePos < SCREEN_WIDTH - 1; linePos += lineDistance) {
+			if (linePos <= SCREEN_WIDTH)
+				DrawLine(linePos, 0, linePos, SCREEN_HEIGHT, GRAY);
+			if (linePos <= SCREEN_HEIGHT)
+				DrawLine(0, linePos, SCREEN_WIDTH, linePos, GRAY);
+		}
+	EndDrawing();
+	}
 }
