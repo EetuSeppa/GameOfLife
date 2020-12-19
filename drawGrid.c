bc819e9b428ad4d55a7e8a531fdf631ff19b61d9
EetuSeppa <eetu.seppanen99@gmail.com>
2020-12-19 13:08:10 +0200

    Added panning relative to world, some mysterious bugs still

--- drawGrid.c
+++ drawGrid.c
@@ -5,43 +5,46 @@
 #define SCREEN_HEIGHT 700
 void drawGrid (void) {		
 	int lineDistance, i, drawnPosX, drawnPosY, worldPosX, worldPosY;
-	int initMouseX, initMouseY, mouseOffsetX, mouseOffsetY;
+	int initMouseX, initMouseY, mouseOffsetX, mouseOffsetY, prevMouseOffsetX, prevMouseOffsetY;
 	lineDistance = 10;
 
 	//Lisää paneroitujen koordinaattien muuttaminen näytölle
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game Of Life");
 
 	mouseOffsetX = mouseOffsetY = 0;
+	worldPosX = worldPosY = 0;
+	prevMouseOffsetX = prevMouseOffsetY = 0;
 	while (!WindowShouldClose()) {
 		
 		//Get mouse movements while pressed
-		//TODO: Implement panning
+		//TODO: Implement position in wolrd 
 		//TODO: Implement zooming
 		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-			initMouseX = GetMouseX();
-			initMouseY = GetMouseY();
+			initMouseX = GetMouseX() - worldPosX;
+			initMouseY = GetMouseY() - worldPosY;
 		}
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+			prevMouseOffsetX = mouseOffsetX;
+			prevMouseOffsetY = mouseOffsetY;
 			mouseOffsetX = GetMouseX() - initMouseX;
 			mouseOffsetY = GetMouseY() - initMouseY;
 		}
 		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-			mouseOffsetX = 0;
-			mouseOffsetY = 0;
+			//prevMouseOffsetX = 0;
+			//prevMouseOffsetY = 0;
 		}
 		
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
 			drawnPosY = drawnPosX = 0;
+			worldPosX += mouseOffsetX - prevMouseOffsetX;
+			worldPosY += mouseOffsetY - prevMouseOffsetY;
 			for (drawnPosX = 0;  drawnPosX < SCREEN_WIDTH - 1; drawnPosX += lineDistance) {
-				worldPosX += mouseOffsetX;
-				worldPosY += mouseOffsetY;
-				if (drawnPosX + mouseOffsetX <= SCREEN_WIDTH) {
-					DrawLine(drawnPosX + mouseOffsetX, mouseOffsetY, drawnPosX + mouseOffsetX, SCREEN_HEIGHT + mouseOffsetY, GRAY);
+				if (drawnPosX <= SCREEN_WIDTH) {
+					DrawLine(worldPosX + drawnPosX, worldPosY, worldPosX + drawnPosX, SCREEN_HEIGHT + worldPosY, GRAY);
 				}
 				if (drawnPosY <= SCREEN_HEIGHT) {
-					DrawLine(mouseOffsetX, drawnPosY + mouseOffsetY, SCREEN_WIDTH  + mouseOffsetX, drawnPosY + mouseOffsetY, GRAY);
-					;
+					DrawLine(worldPosX, worldPosY + drawnPosY, SCREEN_WIDTH  + worldPosX, worldPosY + drawnPosY, GRAY);
 				}
 	
 				drawnPosY += lineDistance;
