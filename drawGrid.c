549b173529974f71837738d5047f8e6aa127b179
EetuSeppa <eetu.seppanen99@gmail.com>
2020-12-19 11:22:43 +0200

    Added panning.

--- drawGrid.c
+++ drawGrid.c
@@ -4,38 +4,45 @@
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 700
 void drawGrid (void) {		
-	int lineDistance, i, screenPosX, screenPosY, worldPosX, worldPosY, drawnPosX, drawnPosY;
-	int initMouseX, initMouseY;
+	int lineDistance, i, drawnPosX, drawnPosY, worldPosX, worldPosY;
+	int initMouseX, initMouseY, mouseOffsetX, mouseOffsetY;
 	lineDistance = 10;
 
 	//Lisää paneroitujen koordinaattien muuttaminen näytölle
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game Of Life");
 
+	mouseOffsetX = mouseOffsetY = 0;
 	while (!WindowShouldClose()) {
 		
 		//Get mouse movements while pressed
 		//TODO: Implement panning
+		//TODO: Implement zooming
 		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 			initMouseX = GetMouseX();
 			initMouseY = GetMouseY();
 		}
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-			
-
+			mouseOffsetX = GetMouseX() - initMouseX;
+			mouseOffsetY = GetMouseY() - initMouseY;
 		}
-		/*if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-			initMouseX = 0;
-			initMouseY = 0;
+		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+			mouseOffsetX = 0;
+			mouseOffsetY = 0;
 		}
-		*/
+		
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
 			drawnPosY = drawnPosX = 0;
 			for (drawnPosX = 0;  drawnPosX < SCREEN_WIDTH - 1; drawnPosX += lineDistance) {
-				if (drawnPosX <= SCREEN_WIDTH)
-					DrawLine(drawnPosX, 0, drawnPosX, SCREEN_HEIGHT, GRAY);
-				if (drawnPosY <= SCREEN_HEIGHT)
-					DrawLine(0, drawnPosY, SCREEN_WIDTH, drawnPosY, GRAY);
+				worldPosX += mouseOffsetX;
+				worldPosY += mouseOffsetY;
+				if (drawnPosX + mouseOffsetX <= SCREEN_WIDTH) {
+					DrawLine(drawnPosX + mouseOffsetX, mouseOffsetY, drawnPosX + mouseOffsetX, SCREEN_HEIGHT + mouseOffsetY, GRAY);
+				}
+				if (drawnPosY <= SCREEN_HEIGHT) {
+					DrawLine(mouseOffsetX, drawnPosY + mouseOffsetY, SCREEN_WIDTH  + mouseOffsetX, drawnPosY + mouseOffsetY, GRAY);
+					;
+				}
 	
 				drawnPosY += lineDistance;
 			}
