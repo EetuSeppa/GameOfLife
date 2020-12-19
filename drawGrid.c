e69958abba6d3c5384e698ac3d208ae065dd6dc0
EetuSeppa <eetu.seppanen99@gmail.com>
2020-12-19 09:56:03 +0200

    Started implementing panning

--- drawGrid.c
+++ drawGrid.c
@@ -3,23 +3,47 @@
 
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 700
-int main (void) {		
-	int lineDistance, i, linePos;
+void drawGrid (void) {		
+	int lineDistance, i, screenPosX, screenPosY, worldPosX, worldPosY, drawnPosX, drawnPosY;
+	int initMouseX, initMouseY;
 	lineDistance = 10;
 
 	//Lisää paneroitujen koordinaattien muuttaminen näytölle
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game Of Life");
 
 	while (!WindowShouldClose()) {
-	
-	BeginDrawing();
-		ClearBackground(RAYWHITE);
-		for (linePos = 0;  linePos < SCREEN_WIDTH - 1; linePos += lineDistance) {
-			if (linePos <= SCREEN_WIDTH)
-				DrawLine(linePos, 0, linePos, SCREEN_HEIGHT, GRAY);
-			if (linePos <= SCREEN_HEIGHT)
-				DrawLine(0, linePos, SCREEN_WIDTH, linePos, GRAY);
+		
+		//Get mouse movements while pressed
+		//TODO: Implement panning
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+			initMouseX = GetMouseX();
+			initMouseY = GetMouseY();
 		}
-	EndDrawing();
+		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+			
+
+		}
+		/*if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+			initMouseX = 0;
+			initMouseY = 0;
+		}
+		*/
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			drawnPosY = drawnPosX = 0;
+			for (drawnPosX = 0;  drawnPosX < SCREEN_WIDTH - 1; drawnPosX += lineDistance) {
+				if (drawnPosX <= SCREEN_WIDTH)
+					DrawLine(drawnPosX, 0, drawnPosX, SCREEN_HEIGHT, GRAY);
+				if (drawnPosY <= SCREEN_HEIGHT)
+					DrawLine(0, drawnPosY, SCREEN_WIDTH, drawnPosY, GRAY);
+	
+				drawnPosY += lineDistance;
+			}
+		EndDrawing();
 	}
+}
+
+int main (void) {
+	drawGrid();
+
 }
